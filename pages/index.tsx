a49b913574c0babbdd1ed3774dd931600e1f2ee6
relluarte <cortinasbras@gmail.com>
2025-11-19 11:24:40 -0300

    chore(next): iniciar migração para Next.js com estrutura básica e scripts

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Bem-vindo ao Next.js!</h1>
+      <p>Primeira página migrada do Vite para Next.js.</p>
+    </main>
+  );
+}
